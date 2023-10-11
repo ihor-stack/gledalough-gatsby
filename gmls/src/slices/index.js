8fe4462fde8b1c7fce5891c5d4dd27b89a8e57c5
Ihor Klymenko <ihorklymenko217@gmail.com>
2023-10-11 21:44:58 +0700

    feat: add cocktail slider

--- gmls/src/slices/index.js
+++ gmls/src/slices/index.js
@@ -2,10 +2,12 @@ import HeroVideo from './HeroVideo'
 import HeroText from './HeroText'
 import StoryArticle from './StoryArticle'
 import StoryPanel from './StoryPanel'
+import CocktailsSlider from './CocktailsSlider'
 
 export const components = {
   herovideo: HeroVideo,
   hero: HeroText,
   stories_section: StoryArticle,
   story_chapters: StoryPanel,
+  cocktailsslider: CocktailsSlider,
 }
--- gmls/src/utils/filters.js
+++ gmls/src/utils/filters.js
@@ -1,3 +1,12 @@
 export const capitilize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export const localUrl = (url) => {
+  const baseUrl = process.env.BASE_URL
+  // if url starts with base url, remove it
+  if (url.startsWith(baseUrl)) {
+    return url.replace(baseUrl, '')
+  }
+  return url
 }
