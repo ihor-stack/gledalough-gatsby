e8c78753f42f406849d67bac680c74d2b1ba39f5
Ihor Klymenko <ihorklymenko217@gmail.com>
2023-10-12 16:54:39 +0700

    feat: add features page

--- gmls/src/templates/features.js
+++ gmls/src/templates/features.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { graphql } from 'gatsby'
 import { withPrismicPreview } from 'gatsby-plugin-prismic-previews'
 import { SliceZone } from '@prismicio/react'
 import { Layout } from '../components/Layout'
+import { components } from '../slices'
 
 const FeaturesTemplate = ({ data }) => {
-
   if (!data) return null
   const pageContent = data.prismicFeatures || {}
 
-  const { meta_title, meta_description, social_card, body: slices } = data.prismicFeatures.data
+  const {
+    meta_title,
+    meta_description,
+    social_card,
+    body: slices,
+  } = data.prismicFeatures.data
 
   const { lang, type, url } = pageContent || {}
   const alternateLanguages = pageContent.alternate_languages || []
@@ -22,8 +27,7 @@ const FeaturesTemplate = ({ data }) => {
 
   return (
     <Layout activeDocMeta={activeDoc}>
-      <div className='page'><h1 style={{color: 'white', marginTop: '8rem'}}>{ meta_title?.text }</h1></div>
-      {/* <SliceZone slices={ slices } components={components} /> */}
+      <SliceZone slices={slices} components={components} />
     </Layout>
   )
 }
@@ -31,28 +35,105 @@ const FeaturesTemplate = ({ data }) => {
 export const query = graphql`
   query featuresQuery($uid: String, $id: String, $lang: String) {
     prismicFeatures(uid: { eq: $uid }, id: { eq: $id }, lang: { eq: $lang }) {
-        _previewable
-        url
-        uid
-        type
+      _previewable
+      url
+      uid
+      type
+      id
+      lang
+      alternate_languages {
         id
+        type
         lang
-        alternate_languages {
-          id
-          type
-          lang
-          uid
+        uid
+      }
+      data {
+        meta_description {
+          richText
+          text
         }
-        data {
-          meta_description {
-            richText
-            text
+        meta_title {
+          richText
+          text
+        }
+        body {
+          ... on PrismicFeaturesDataBodyFeatureslider {
+            id
+            items {
+              slide_date
+              slide_image {
+                url
+                alt
+              }
+              slide_title {
+                text
+              }
+              slide_url {
+                url
+                uid
+                id
+              }
+            }
+            primary {
+              background_color
+              category {
+                text
+              }
+            }
+            slice_label
+            slice_type
           }
-          meta_title {
-            richText
-            text
+          ... on PrismicFeaturesDataBodyHerovideo {
+            id
+            slice_label
+            slice_type
+            primary {
+              background_image {
+                alt
+                url
+              }
+              heading {
+                text
+              }
+              title {
+                text
+              }
+              video_url {
+                url
+                uid
+                id
+              }
+            }
+          }
+          ... on PrismicFeaturesDataBodyStoriesSection {
+            id
+            items {
+              slider_image {
+                url
+                alt
+              }
+            }
+            primary {
+              link {
+                id
+                url
+                uid
+              }
+              heading {
+                text
+              }
+              link_text {
+                text
+              }
+              title {
+                text
+              }
+            }
+            slice_label
+            slice_type
           }
         }
+      }
     }
   }
 `
